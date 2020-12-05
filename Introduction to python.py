2bfc5baac0d6221cf0e4c2aded9ff2c53f40ca92
kushal pokhrel <71119638+thekushalpokhrel@users.noreply.github.com>
2020-12-05 12:39:15 +1100

    Add files via upload

---
+++ Introduction to python.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+import matploitlib.pyplot as plt
+
+
+# In[5]:
+
+
+import pandas as pd
+import numpy as np
+import matploitlib.pyplot as plt
+
+
+# In[11]:
+
+
+df = pd.read_csv('C:/Users/user/Downloads/Effects-of-COVID-19-on-trade-1-February-25-November-2020-provisional.csv')
+
+
+# In[12]:
+
+
+df
+
+
+# In[14]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[15]:
+
+
+df = pd.read_csv('C:/Users/user/Downloads/Effects-of-COVID-19-on-trade-1-February-25-November-2020-provisional.csv')
+
+
+# In[16]:
+
+
+df
+
+
+# In[17]:
+
+
+print (df.head())
+
+
+# In[19]:
+
+
+print (df.head())
+
+
+# In[20]:
+
+
+print (df.shape)
+
+
+# In[21]:
+
+
+print (df.columns)
+
+
+# In[23]:
+
+
+print (df.dtypes)
+
+
+# In[24]:
+
+
+print (df.info)
+
+print (df.info())
+# In[28]:
+
+
+country_df = df.['country']
+
+
+# In[31]:
+
+
+print country_df = df.['country']
+
+
+# In[30]:
+
+
+print (country_df = df.head())
+
+
+# In[ ]:
+
+
+
+
